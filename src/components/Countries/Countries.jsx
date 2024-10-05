8fe0ea589b5675cc5a12909bf58a64c255631033
Abadat Hossain <abadat26@gmail.com>
2024-10-05 22:15:09 +0600

    event handeler added successfully

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,6 +5,11 @@ import '../Country/Country.css'
 export default function Countries() {
 
     const [countries, setCountries]=useState([])
+    const [visitedCountries, setVisitedCountries]=useState([])
+
+    const handleVisitedCountries=(country)=>{
+      console.log(country)
+    }
 
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
@@ -18,7 +23,9 @@ export default function Countries() {
       <div>Countries:{countries.length}</div>
      <div className="countries">
      {
-        countries.map(country=><Country country={country} key={country.id} ></Country>)
+        countries.map(country=><Country country={country} 
+          handleVisitedCountries={handleVisitedCountries}
+          key={country.id} ></Country>)
       }
      </div>
     </>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react';
 import './Country.css'
-export default function Country({country}) {
+export default function Country({country,handleVisitedCountries}) {
     // console.log(country)
     const {name,area,flags}=country;
 
     const [visited, setVisited]=useState(false)
-    
+
     const handleVisited=()=>{
       setVisited(!visited)
     }
   return (
-    <div className="country">
+    <div className={`country ${visited && 'visited'}`}>
+    {/* <div className="country"> */}
       <img src={flags.png} alt="" />
-        <h3 >Country:{name.common}</h3>
+        <h3 style={{color:visited?'purple':'black'}}>Country:{name.common}</h3>
         <h3 >Country:{area}</h3>
+        <button onClick={()=>handleVisitedCountries(country)}>Mark Visited</button><br /><br />
         <button onClick={handleVisited}>{ visited ? 'Visited':'Not Visited'}</button>
         {visited?'visited':'Not visited'}
     </div>
